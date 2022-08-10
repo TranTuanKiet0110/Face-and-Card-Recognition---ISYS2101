fb62839ea748d9447030ad34b84109cd75d056c0
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2022-08-10 10:09:03 +0700

    combinding the data of id card and face

--- scan id card/scan-id-card.py
+++ scan id card/scan-id-card.py
@@ -2,9 +2,9 @@ import cv2
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-img = cv2.imread('Hoang1.png')
+img = cv2.imread('../images file/s3879300/id.jpg')
 #img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 print(pytesseract.image_to_string(img))
 #print(pytesseract.image_to_data(img))
-cv2.imshow('Result' ,img)
+cv2.imshow('Result', img)
 cv2.waitKey(0)
