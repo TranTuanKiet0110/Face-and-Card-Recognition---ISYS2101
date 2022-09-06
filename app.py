b5aefe7a9e91ad636d9349e08135d1f26a52a201
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2022-09-06 12:06:17 +0700

    change pytesseract path and create Aptfile to deploy to heroku

--- app.py
+++ app.py
@@ -141,8 +141,8 @@ def uploadID():
 
         image = request.files['file']
 
-        pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'  # pytesseract location
-
+        # pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'  # pytesseract location
+        pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'  # pytesseract location
         if image.filename == '':
             return redirect(request.url)
 
