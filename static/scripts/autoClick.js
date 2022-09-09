39a21aa28f68fd582319a2e3b0dd0b16569244b9
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2022-09-09 21:21:02 +0700

    edit some css files and add count down in successValidation.html

--- static/scripts/autoClick.js
+++ static/scripts/autoClick.js
@@ -1,5 +1,17 @@
-setTimeout("CallButton()",10000)
-function CallButton()
-{
-   document.getElementById("button1").click();   
-}
+// setTimeout("CallButton()",10000)
+// function CallButton()
+// {
+//    document.getElementById("button1").click();   
+// }
+
+var timeleft = 10;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("button1").innerHTML = "Next in (0s)";
+    document.getElementById("button1").click(); 
+  } else {
+    document.getElementById("button1").innerHTML = "Next in (" + timeleft + "s)";
+  }
+  timeleft -= 1;
+}, 1000);
